8ef48b102be4c8644830e2e9ade5b96e25ceb9cf
nourou21 <qabdenourouhba@gmail.com>
2024-04-23 18:47:32 +0100

    cc

--- lib/inside app/Control.dart
+++ lib/inside app/Control.dart
@@ -21,6 +21,8 @@ class _ControlPageState extends State<ControlPage> {
   Color _fanButtonColor = Colors.red;
   Color _windowButtonColor = Colors.red;
   Color _backgroundColor = Colors.blueGrey.withOpacity(0.2);
+  bool _isGasWarningShown = false;
+  bool _isManualMode = false;
 
   @override
   void initState() {
@@ -100,55 +102,195 @@ class _ControlPageState extends State<ControlPage> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData &&
                       snapshot.data!.snapshot.value != null) {
-                    return Text(
-                      ' ${snapshot.data!.snapshot.value}',
-                      style: TextStyle(fontSize: 20),
-                    );
-                  } else {
-                    return Text(
-                      'Loading...',
-                      style: TextStyle(fontSize: 20),
-                    );
+                    var lcdValue =
+                        int.tryParse(snapshot.data!.snapshot.value.toString());
+                    if (lcdValue != null) {
+                      if (lcdValue > 200) {
+                        return Text(
+                          'Danger: $lcdValue',
+                          style: TextStyle(fontSize: 20, color: Colors.red),
+                        );
+                      } else {
+                        return Text(
+                          'Value: $lcdValue',
+                          style: TextStyle(fontSize: 20),
+                        );
+                      }
+                    }
                   }
+                  return Text(
+                    'Loading...',
+                    style: TextStyle(fontSize: 20),
+                  );
                 },
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _toggleManualMode(true),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    backgroundColor: _isManualMode ? Colors.green : Colors.grey,
+                  ),
+                  child: Text(
+                    'Manual',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => _toggleManualMode(false),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    backgroundColor:
+                        !_isManualMode ? Colors.green : Colors.grey,
+                  ),
+                  child: Text(
+                    'Auto',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
             ),
             Expanded(
               child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SizedBox(height: 400),
+                    SizedBox(height: 300),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            StreamBuilder(
+                              stream:
+                                  _database.child('state/fan state').onValue,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData &&
+                                    snapshot.data!.snapshot.value != null) {
+                                  bool isFanOn =
+                                      snapshot.data!.snapshot.value == 'on';
+                                  return GestureDetector(
+                                    onTap: () {}, // Disable onTap functionality
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 20, horizontal: 40),
+                                      color:
+                                          isFanOn ? Colors.green : Colors.red,
+                                      child: Text(
+                                        isFanOn ? 'Fan is On' : 'Fan is Off',
+                                        style: TextStyle(
+                                            fontSize: 18, color: Colors.white),
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return CircularProgressIndicator();
+                                }
+                              },
+                            ),
+                            SizedBox(width: 20),
+                            StreamBuilder(
+                              stream:
+                                  _database.child('state/servo state').onValue,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData &&
+                                    snapshot.data!.snapshot.value != null) {
+                                  bool areWindowsOpen =
+                                      snapshot.data!.snapshot.value == 'open';
+                                  return GestureDetector(
+                                    onTap: () {}, // Disable onTap functionality
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 20, horizontal: 38),
+                                      color: areWindowsOpen
+                                          ? Colors.green
+                                          : Colors.red,
+                                      child: Text(
+                                        areWindowsOpen
+                                            ? 'Windows Open'
+                                            : 'Windows Closed',
+                                        style: TextStyle(
+                                            fontSize: 18, color: Colors.white),
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return CircularProgressIndicator();
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         ElevatedButton(
-                          onPressed: () => _toggleFanState(),
+                          onPressed: () => _toggleFanState(true),
                           style: ElevatedButton.styleFrom(
                             padding: EdgeInsets.symmetric(
                                 vertical: 20, horizontal: 40),
-                            backgroundColor: _fanButtonColor,
+                            backgroundColor: Colors.green,
                           ),
                           child: Text(
-                            _isFanOn ? 'Fan is On' : 'Fan is Off',
+                            'Turn Fan On',
                             style: TextStyle(fontSize: 18),
                           ),
                         ),
                         SizedBox(width: 20),
                         ElevatedButton(
-                          onPressed: () => _toggleWindowState(),
+                          onPressed: () => _toggleFanState(false),
                           style: ElevatedButton.styleFrom(
                             padding: EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 38),
-                            backgroundColor: _windowButtonColor,
+                                vertical: 20, horizontal: 40),
+                            backgroundColor: Colors.red,
                           ),
                           child: Text(
-                            _areWindowsOpen ? 'Windows Open' : 'Windows Closed',
+                            'Turn Fan Off',
                             style: TextStyle(fontSize: 18),
                           ),
                         ),
                       ],
                     ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () => _toggleWindowState(true),
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 38),
+                            backgroundColor: Colors.green,
+                          ),
+                          child: Text(
+                            'Open Windows',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        ElevatedButton(
+                          onPressed: () => _toggleWindowState(false),
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 33),
+                            backgroundColor: Colors.red,
+                          ),
+                          child: Text(
+                            'Close Windows',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
@@ -159,9 +301,9 @@ class _ControlPageState extends State<ControlPage> {
     );
   }
 
-  Future<void> _toggleFanState() async {
+  Future<void> _toggleFanState(bool newState) async {
     try {
-      var newFanState = _isFanOn ? 'off' : 'on';
+      var newFanState = newState ? 'on' : 'off';
       await _database.child('state/fan').set(newFanState);
       print('Fan state updated to: $newFanState');
     } catch (e) {
@@ -169,9 +311,9 @@ class _ControlPageState extends State<ControlPage> {
     }
   }
 
-  Future<void> _toggleWindowState() async {
+  Future<void> _toggleWindowState(bool newState) async {
     try {
-      var newWindowState = _areWindowsOpen ? 'close' : 'open';
+      var newWindowState = newState ? 'open' : 'close';
       await _database.child('state/Servo').set(newWindowState);
       print('Windows state updated to: $newWindowState');
     } catch (e) {
@@ -179,16 +321,32 @@ class _ControlPageState extends State<ControlPage> {
     }
   }
 
+  Future<void> _toggleManualMode(bool isManual) async {
+    try {
+      _isManualMode = isManual;
+      await _database.child('state/manual').set(isManual);
+      print('Mode set to ${isManual ? 'Manual' : 'Auto'}');
+      setState(() {}); // Refresh the UI to reflect the mode change
+    } catch (e) {
+      print('Error setting mode: $e');
+    }
+  }
+
   void _updateBackgroundColor() {
     _database.child('state/lcd').onValue.listen((event) {
       if (event.snapshot.value != null) {
         var lcdValue = int.tryParse(event.snapshot.value.toString());
-        if (lcdValue != null && lcdValue >= 200) {
+        if (lcdValue != null && lcdValue >= 200 && !_isGasWarningShown) {
+          // Show alert only if it hasn't been shown before
           setState(() {
             _backgroundColor = Colors.red.withOpacity(0.5);
             _showGasWarningDialog();
+            _isGasWarningShown =
+                true; // Set the flag to true to indicate that the notification has been shown
           });
-        } else {
+        } else if (lcdValue != null && lcdValue < 200) {
+          // Reset the flag when the gas level goes below 200
+          _isGasWarningShown = false;
           setState(() {
             _backgroundColor = Colors.blueGrey.withOpacity(0.2);
           });
